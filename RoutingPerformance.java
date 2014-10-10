82d25c73e1a37de136b345537b72370e7833bde2
tnguyenbluesat <tnguyen@bluesat.unsw.edu.au>
2014-10-10 13:47:21 +1100

    added workload.java

--- RoutingPerformance.java
+++ RoutingPerformance.java
@@ -9,6 +9,7 @@ public class RoutingPerformance
 {
    //private static Path topology_file;
    private static Graph network_topology;
+   private static Workload workload;
    public static void main(String[] args) throws Exception
    {
       // Get command line argument.
@@ -17,6 +18,8 @@ public class RoutingPerformance
       //    return;
       // }
    	network_topology = new Graph(args[0]);
+   	workload = new Workload(args[1]);
+   	workload.element().print();
       network_topology.get("A").get("B").print();
       network_topology.get("J").get("K").print();
    }
---
+++ Workload.java
@@ -0,0 +1,75 @@
+import java.util.*;
+import java.io.*;
+//import java.util.Queue;
+public class Workload
+{
+	private Queue<Request> allRequests;
+
+	public Workload()
+	{
+		 System.out.println("WOOO");
+	}
+	
+	public Workload (String filename) throws IOException
+	{
+		allRequests = new LinkedList<Request>();
+		BufferedReader instream = new BufferedReader(new FileReader(filename));
+		String inputLine;
+		Request currRequest;
+		while(instream.ready())
+		{
+			inputLine = instream.readLine();
+			currRequest = parse(inputLine);
+			allRequests.add(currRequest);
+		}
+		
+		instream.close();
+	}
+	public Request parse (String line)
+	{
+		Request request = new Request();
+		
+		String params[] = line.split(" ");
+	
+		request.timestamp = Float.parseFloat(params[0]);
+		request.source = params[1];
+		request.dest = params[2];
+		request.duration = Float.parseFloat(params[3]);
+		
+		return request;
+	}
+	
+	public void add(Request _request)
+	{
+		allRequests.add(_request);
+	}
+	public Request remove()
+	{
+		return allRequests.remove();
+	}
+	public Request poll()
+	{
+		return allRequests.poll();
+	}
+	public Request element()
+	{
+		return allRequests.element();
+	}
+		
+}
+final class Request
+{
+	float timestamp;
+	String dest;
+	String source;
+	float duration;
+	
+	public void print()
+	{
+		System.out.println("Request Timestamp: " + timestamp +
+				"|Source: " + source +
+				"|Dest: " + dest + 
+				"|Duration: "+ duration);
+	}
+}
+//hash->hash->(name, delay, capacity)
